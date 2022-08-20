e32f9018563a28bd0b89eb8110d2a1b7f24e55b0
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-21 00:39:33 +0200

    add theme function

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -1,8 +1,14 @@
 const themeButton = document.getElementById('theme-button');
+const body = document.body;
+const todoApp = document.querySelector('.todo-app');
 
 export const toggleTheme = () => {
     themeButton.addEventListener('click', () => {
         themeButton.classList.toggle('dark');
         themeButton.classList.toggle('light');
+        body.classList.toggle('dark');
+        body.classList.toggle('light');
+        todoApp.classList.toggle('dark');
+        todoApp.classList.toggle('light');
     });
 };
