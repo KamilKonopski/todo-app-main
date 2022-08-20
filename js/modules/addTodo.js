00fdc45e18fc7f3ca0fea0abffe9987414c23326
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-21 01:53:18 +0200

    add add-todo function

--- js/modules/addTodo.js
+++ js/modules/addTodo.js
@@ -1,16 +1,14 @@
-
-const addButton = document.getElementById('add-button');
-
 const addNewTodo = () => {
-    addButton.addEventListener('click', () => {
-        const addTodoInput = document.getElementById('add-todo');
-    
-        if(addTodoInput.value !== '') {
-        const divElement = document.createElement('div');
-        divElement.innerText = addTodoInput.value;
-        document.querySelector('.todo-app').appendChild(divElement);
-        
-        addTodoInput.value = "";
+    const addTodoInput = document.getElementById('add-todo');
+    addTodoInput.addEventListener('keypress', (e) => {
+        if(e.key === "Enter") {
+            if(addTodoInput.value !== '') {
+            const divElement = document.createElement('div');
+            divElement.innerText = addTodoInput.value;
+            document.querySelector('.todo-app').appendChild(divElement);
+            
+            addTodoInput.value = "";
+            }
         }
     });
 };
