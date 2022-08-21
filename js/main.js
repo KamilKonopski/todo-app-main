d63359452696371dc9e75344121dcbd0ca213af4
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-21 22:24:54 +0200

    delete function

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ import { addNewTodo } from "./modules/addTodo.js";
 toggleTheme()
 
 addNewTodo()
+
+// delete 
+
+// const deleteButtons = document.getElementsByClassName('test-delete');
+
+
+// function deleteTodo() {
+//     this.parentElement.remove()
+// }
+
+// for(let i = 0; i < deleteButtons.length; i++) {
+//     deleteButtons[i].addEventListener('click', deleteTodo)
+// };
+
