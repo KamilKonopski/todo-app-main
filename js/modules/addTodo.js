1ac1f5105f0a6aa559af22e06039ad2642cb0aec
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-22 02:05:41 +0200

    add function to addtodo before filters todo

--- js/modules/addTodo.js
+++ js/modules/addTodo.js
@@ -4,8 +4,24 @@ const addNewTodo = () => {
         if(e.key === "Enter") {
             if(addTodoInput.value !== '') {
             const divElement = document.createElement('div');
-            divElement.innerText = addTodoInput.value;
-            document.querySelector('.todo-app').appendChild(divElement);
+            divElement.classList.add('simple-todo');
+            document.querySelector('.todo-list').appendChild(divElement);
+
+            const filtersDiv = document.querySelector('.filters-todo');
+            divElement.parentNode.insertBefore(divElement, filtersDiv);
+
+            const doneButton = document.createElement('button');
+            doneButton.classList.add('done-todo');
+            divElement.appendChild(doneButton);
+            
+            const textElement = document.createElement('p');
+            textElement.classList.add('text-todo');
+            textElement.innerText = addTodoInput.value;
+            divElement.appendChild(textElement);
+            
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete-todo');
+            divElement.appendChild(deleteButton);
             
             addTodoInput.value = "";
             }
