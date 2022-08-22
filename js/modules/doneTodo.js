0f1746715a7972902eb1cef37f1f6798dd6a748c
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-22 03:27:52 +0200

    add done function

--- js/modules/doneTodo.js
+++ js/modules/doneTodo.js
@@ -1,6 +1,20 @@
-// const doneTestButton = document.querySelector('.test-button');
+export const doneTodo = () => {
+    const doneButtons = document.getElementsByClassName('done-todo');
+    
+    for(let i = 0; i < doneButtons.length; i++) {
+        doneButtons[i].addEventListener('click', () => {
+            const nextElement = doneButtons[i].nextElementSibling;
+            console.log(nextElement)
+            nextElement.classList.toggle('done')
+        })
+    }
+
+}
+
 
 // doneTestButton.addEventListener('click', () => {
 //     const nextELement = doneTestButton.nextElementSibling;
 //     nextELement.classList.toggle('done');
 // })
+
+
