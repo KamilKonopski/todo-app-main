44d55cdf7d34864400109e65eead18db80d79b06
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-22 02:46:45 +0200

    test function to add todo list element class done

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ import { deleteTodo } from "./modules/deleteTodo.js";
 toggleTheme()
 
 addNewTodo()
+
+// add done in todo list element
+
+// const doneTestButton = document.querySelector('.test-button');
+
+// doneTestButton.addEventListener('click', () => {
+//     const nextELement = doneTestButton.nextElementSibling;
+//     nextELement.classList.toggle('done');
+// })
