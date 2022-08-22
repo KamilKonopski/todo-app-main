4ed9051f9c84f28fe3a8afd3b6092e4445d95338
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-22 03:22:25 +0200

    delete example function

--- js/main.js
+++ js/main.js
@@ -4,12 +4,3 @@ import { addNewTodo } from "./modules/addTodo.js";
 toggleTheme()
 
 addNewTodo()
-
-// add done in todo list element
-
-// const doneTestButton = document.querySelector('.test-button');
-
-// doneTestButton.addEventListener('click', () => {
-//     const nextELement = doneTestButton.nextElementSibling;
-//     nextELement.classList.toggle('done');
-// })
