904a9686f5c53ac81f568673315c0a38592ff58c
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-08-24 14:51:06 +0200

    add add todo, completed and delete functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,61 @@
-import { toggleTheme } from "./modules/theme.js";
-import { addNewTodo } from "./modules/addTodo.js"; 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = document.getElementsByClassName('dark');
+const lightTheme = document.getElementsByClassName('light');
+const addTodoInput = document.getElementById('add-todo');
+const todoList = document.querySelector('.todo-list');
 
-toggleTheme()
+themeButton.addEventListener('click', () => {
+    if(themeButton.classList.contains('dark')) {
+        for(let i = 0; i = darkTheme.length; i++) {
+            darkTheme[0].className = darkTheme[0].className.replace('dark', 'light');
+        };
+    } else if(themeButton.classList.contains('light')) {
+        for(let i = 0; i = lightTheme.length; i++) {
+            lightTheme[0].className = lightTheme[0].className.replace('light', 'dark');
+        }; 
+    };
+});
 
-addNewTodo()
+addTodoInput.addEventListener('keypress', (e) => {
+    if(e.key === "Enter") {
+        if(addTodoInput.value !== '') {
+        const divElement = document.createElement('div');
+        divElement.classList.add('simple-todo');
+        document.querySelector('.todo-list').appendChild(divElement);
+
+        const filtersDiv = document.querySelector('.filters-todo');
+        divElement.parentNode.insertBefore(divElement, filtersDiv);
+
+        const doneButton = document.createElement('button');
+        doneButton.classList.add('done-todo');
+        divElement.appendChild(doneButton);
+        
+        const textElement = document.createElement('p');
+        textElement.classList.add('text-todo');
+        textElement.innerText = addTodoInput.value;
+        divElement.appendChild(textElement);
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-todo');
+        divElement.appendChild(deleteButton);
+        
+        addTodoInput.value = "";
+        }
+    }
+    
+});
+
+todoList.addEventListener('click', e => {
+    const item = e.target;
+    if(item.classList[0] === 'delete-todo') {
+        item.parentElement.remove();
+    }
+
+    if(item.classList[0] === 'done-todo') {
+        const nextElement = item.nextElementSibling;
+        nextElement.classList.toggle('completed');
+        console.log(item);
+        item.classList.toggle('completed');
+        item.parentElement.classList.toggle('completed')
+    }
+})
--- js/modules/deleteTodo.js
+++
@@ -1,9 +0,0 @@
-export const deleteTodo = () => {
-    const deleteButtons = document.getElementsByClassName('delete-todo');
-    for(let i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener('click', function() {
-            this.parentElement.remove()
-        })
-    };
-}
-
