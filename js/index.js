f3ce33b4ee1f94638cfec18160620d45ff96c875
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-01 22:32:08 +0200

    add interface

--- js/index.js
+++ js/index.js
@@ -1,45 +1,9 @@
-"use strict";
+import { uuid } from "uuidv4";
 //add todo variables
 const addTodoForm = document.querySelector(".add-todo-form");
 const addTodoInput = document.querySelector("#add-todo");
 //todo list variables
 const todoList = document.querySelector(".todo-list__containter");
 //todos array
-let todos = [];
-// functions
-const addTodo = (todo) => {
-    todos.push(todo);
-};
-const renderTodoList = () => {
-    if (todos.length) {
-        todoList.innerHTML = "";
-        todos.map((todo) => {
-            const todoListContainer = document.createElement("li");
-            todoListContainer.classList.add("simple-todo");
-            const completedButtonElement = document.createElement("button");
-            completedButtonElement.classList.add("done-todo");
-            todoListContainer.appendChild(completedButtonElement);
-            const spanElement = document.createElement("span");
-            spanElement.classList.add("text-todo");
-            spanElement.innerText = todo;
-            todoListContainer.appendChild(spanElement);
-            const deleteButtonELement = document.createElement("button");
-            deleteButtonELement.classList.add("delete-todo");
-            todoListContainer.appendChild(deleteButtonELement);
-            todoList === null || todoList === void 0 ? void 0 : todoList.appendChild(todoListContainer);
-        });
-    }
-    else
-        return;
-};
-// listeners
-addTodoForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (addTodoInput.value !== "") {
-        addTodo(addTodoInput.value);
-        addTodoInput.value = "";
-        renderTodoList();
-    }
-});
-//first todo list render
-renderTodoList();
+let todos;
+console.log(uuid());
--- ts/index.ts
+++ ts/index.ts
@@ -5,54 +5,13 @@ const addTodoInput = document.querySelector("#add-todo") as HTMLInputElement;
 //todo list variables
 const todoList = document.querySelector(
 	".todo-list__containter"
-) as HTMLDivElement;
+) as HTMLUListElement;
+
+interface ArrayTodoList {
+	id: string;
+	todo: string;
+	done: boolean;
+}
 
 //todos array
-let todos: string[] = [];
-
-// functions
-const addTodo = (todo: string) => {
-	todos.push(todo);
-};
-
-const renderTodoList = () => {
-	if (todos.length) {
-		todoList.innerHTML = "";
-
-		todos.map((todo) => {
-			const todoListContainer = document.createElement("li");
-			todoListContainer.classList.add("simple-todo");
-
-			const completedButtonElement = document.createElement("button");
-			completedButtonElement.classList.add("done-todo");
-			todoListContainer.appendChild(completedButtonElement);
-
-			const spanElement = document.createElement("span");
-			spanElement.classList.add("text-todo");
-			spanElement.innerText = todo;
-			todoListContainer.appendChild(spanElement);
-
-			const deleteButtonELement = document.createElement("button");
-			deleteButtonELement.classList.add("delete-todo");
-			todoListContainer.appendChild(deleteButtonELement);
-
-			todoList?.appendChild(todoListContainer);
-		});
-	} else return;
-};
-
-// listeners
-addTodoForm.addEventListener("submit", (event: Event) => {
-	event.preventDefault();
-
-	if (addTodoInput.value !== "") {
-		addTodo(addTodoInput.value);
-
-		addTodoInput.value = "";
-
-		renderTodoList();
-	}
-});
-
-//first todo list render
-renderTodoList();
+let todos: ArrayTodoList[];
